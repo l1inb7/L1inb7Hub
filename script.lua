431bb718ebddc9bd76e836e8882631c7741822b6
l1inb7 <k37381406@gmail.com>
2026-03-09 02:20:48 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,9 @@
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "+1 Speed Escape Skateboard [OP & FREE]",
-   LoadingTitle = "🎲 L1inb7 Hub",
-   LoadingSubtitle = "by L1inb7",
+   Name = "L1inb7 Hub",
+   LoadingTitle = "L1inb7 Hub",
+   LoadingSubtitle = "Scr1pt L0ader",
    ConfigurationSaving = {
       Enabled = false,
       FileName = "L1inb7Hub"
@@ -11,125 +11,50 @@ local Window = Rayfield:CreateWindow({
    KeySystem = false
 })
 
-local player = game.Players.LocalPlayer
-
 ----------------------------------------------------
--- MAIN TAB
+-- SCRIPTS TAB
 ----------------------------------------------------
 
-local MainTab = Window:CreateTab("Main", 4483362458)
+local ScriptsTab = Window:CreateTab("Scripts", 4483362458)
 
-Rayfield:Notify({
-   Title = "You executed the script",
-   Content = "Thanks for using L1inb7 Hub!",
-   Duration = 5
+-- SCRIPT 1
+ScriptsTab:CreateButton({
+   Name = "+1 Speed Escape Skateboard Script",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/l1inb7/L1inb7Hub-1SpeedEscapeSkateboard/main/script.lua"))()
+   end,
 })
 
-----------------------------------------------------
--- AUTO WINS
-----------------------------------------------------
+-- SCRIPT 2
+ScriptsTab:CreateButton({
+   Name = "Soon...",
+   Callback = function()
+      loadstring(game:HttpGet("PASTE_YOUR_RAW_LINK_HERE"))()
+   end,
+})
 
-local autowins = false
-
-MainTab:CreateToggle({
-   Name = "Auto Claim Wins",
-   CurrentValue = false,
-   Callback = function(Value)
-      autowins = Value
-
-      task.spawn(function()
-         while autowins do
-
-            local args = {
-               [1] = "Free",
-               [2] = Vector3.new(9914.46875, 6.3058, -5766.1577)
-            }
-
-            game:GetService("ReplicatedStorage")
-            .Remote.Event.Player.ClaimWins
-            :FireServer(unpack(args))
-
-            task.wait(1)
-
-         end
-      end)
+-- SCRIPT 3
+ScriptsTab:CreateButton({
+   Name = "Soon..",
+   Callback = function()
+      loadstring(game:HttpGet("PASTE_YOUR_RAW_LINK_HERE"))()
    end,
 })
 
 ----------------------------------------------------
--- EQUIP BEST SKATEBOARD
+-- INFO TAB
 ----------------------------------------------------
 
-MainTab:CreateButton({
-   Name = "Equip Best Skateboard (Northern Star)",
+local InfoTab = Window:CreateTab("Info", 4483362458)
+
+InfoTab:CreateButton({
+   Name = "Copy YouTube",
    Callback = function()
-
-      local args = {
-         [1] = "Northern Star"
-      }
-
-      game:GetService("ReplicatedStorage")
-      .Remote.Event.UI.EquipBoard
-      :FireServer(unpack(args))
-
+      setclipboard("https://youtube.com/@L1inb7_Scriptss")
       Rayfield:Notify({
          Title = "L1inb7 Hub",
-         Content = "Northern Star Equipped!",
+         Content = "YouTube copied!",
          Duration = 4
-      })
-
-   end,
-})
-
-----------------------------------------------------
--- OPEN EGG
-----------------------------------------------------
-
-MainTab:CreateButton({
-   Name = "Open Egg",
-   Callback = function()
-
-      local args = {
-         [1] = "Egg4"
-      }
-
-      game:GetService("ReplicatedStorage")
-      .Remote.Function.Eggs.OpenEgg
-      :InvokeServer(unpack(args))
-
-      Rayfield:Notify({
-         Title = "L1inb7 Hub",
-         Content = "Egg opened!",
-         Duration = 4
-      })
-
-   end,
-})
-
-----------------------------------------------------
--- MESSAGE TAB
-----------------------------------------------------
-
-local MsgTab = Window:CreateTab("💬 Message", 4483362458)
-
-MsgTab:CreateButton({
-   Name = "Ty for testing my scripts",
-   Callback = function()
-      Rayfield:Notify({
-         Title = "L1inb7 Hub",
-         Content = "Thanks for testing ❤️",
-         Duration = 5
-      })
-   end,
-})
-
-MsgTab:CreateButton({
-   Name = "My YT is L1inb7_Scriptss",
-   Callback = function()
-      Rayfield:Notify({
-         Title = "L1inb7 Hub",
-         Content = "Subscribe on YouTube!",
-         Duration = 5
       })
    end,
 })
